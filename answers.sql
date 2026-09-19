aed2241c9034d863d002b383a4d0c446474aacdb
Jothi-SCS <54353240+Jothi-SCS@users.noreply.github.com>
2026-09-19 12:48:13 +0530

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -1,16 +1,1 @@
-SET SERVEROUTPUT ON;
 
-ACCEPT marks NUMBER PROMPT 'Enter student marks: '
-
-DECLARE
-    v_marks NUMBER := &marks;
-BEGIN
-
-    IF v_marks >= 40 THEN
-        DBMS_OUTPUT.PUT_LINE('PASS');
-    ELSE
-        DBMS_OUTPUT.PUT_LINE('FAIL');
-    END IF;
-
-END;
-/
